c8c992748ee140bb758675418033bd90f64511f8
anantakhanal16 <115576263+anantakhanal16@users.noreply.github.com>
2024-02-07 11:29:30 +0545

    Add files via upload

---
+++ Media tr and Cqrs/Media tr and Cqrs/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Media_tr_and_Cqrs.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Media tr and Cqrs/Media tr and Cqrs/Data/AppDataContext.cs
@@ -0,0 +1,11 @@
+﻿using Media_tr_and_Cqrs.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Media_tr_and_Cqrs.Data
+{
+    public class AppDataContext:DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+    }
+
+}
